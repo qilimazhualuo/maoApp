fc47262e6b18f46a8b899e0eb8ca74bf20262691
qilimazhualuo <49083614+qilimazhualuo@users.noreply.github.com>
2024-12-20 17:17:54 +0800

    update

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { NavBar } from 'antd-mobile'
+import { NavBar, Button, SideBar } from 'antd-mobile'
 import { invoke } from '@tauri-apps/api/core'
 import { Trans } from 'react-i18next'
 
@@ -8,35 +8,35 @@ import Scene from '@/common/three'
 import './App.less'
 
 function App() {
-    let map, scene
+    const [navVisible, setNavVisible] = useState(false)
+    let scene
     useEffect(() => {
-        map = new Map({
-            id: 'map',
-            center: [120, 30],
-            zoom: 10
-        })
-        map.loadMap('gaode')
-        map.addEvent('click', (e) => {
-            console.log(e.coordinate)
-        })
         scene = new Scene({
             dom: 'scene',
         })
-        scene.addBox({ width: 100, height: 100, depth: 100 })
+        scene.addBox({ width: 100, height: 100, depth: 100, color: "#fff000" })
         scene.goView({ x: 0, y: 0, z: 0 }, 200)
         return () => {
-            map && map.destroy()
-            scene && scene.destroy()
+            scene && scene.dispose()
         }
     }, [])
     return (
         <main className="container">
             <NavBar
                 back={null}
-                right={<Trans i18nKey="title" />}
+                right={<Button
+                    onClick={() => setNavVisible(!navVisible)}>
+                    <Trans i18nKey="title" />
+                </Button>}
             >猫猫</NavBar>
-            <div className="scene" id="map"></div>
-            <div className="scene" id="scene"></div>
+            <div className="content">
+                <SideBar className={{ sideNav: true, navVisible }} >
+                    <SideBar.Item key='1' title="仓库" />
+                    <SideBar.Item key='2' title="资源" />
+                    <SideBar.Item key='3' title="仓库" />
+                </SideBar>
+                <div className="scene" id="scene"></div>
+            </div>
         </main>
     )
 }
